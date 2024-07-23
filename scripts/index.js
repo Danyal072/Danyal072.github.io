f04a62859e0dfed58cfed76eebd9b10c934e75b3
Danyal072 <Danyala07@proton.me>
2024-07-23 21:52:39 +0500

    Add Fonts

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,9 @@
-var navbar = document.querySelector('#navbar');
-
-
-function stickyNavbar() {
-  
-    if (window.scrollY > 0) {
-        navbar.style.postion = 'fixed';
-        console.log('scrolling');
-    } else {
-        navbar.style.postion = 'relative';
-    }
-    
-}
-
-stickyNavbar();
+window.addEventListener('scroll', function() {
+    var scroll = document.querySelector('#navbar');
+    var burger = document.querySelector('.label');
+    var a = document.querySelector('.burger');
+    scroll.classList.toggle("sticky", window.scrollY > 0);
+     burger.classList.toggle("wow", window.scrollY > 0);
+     a.classList.toggle("wow", window.scrollY > 0);
+    // b.classList.toggle("lol", window.scrollY > 0);
+});
